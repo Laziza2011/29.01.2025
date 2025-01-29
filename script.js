bd55a415cc36a6fe6fb4b274dcde73b1acf6b878
Laziza <laziza@.com>
2025-01-29 15:00:16 +0300

    student

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let text = document.getElementById("pId");
-student = { sinf: 8, harf: "A", ustoz: true };
+student = { sinf: 8, harf: "A", ism:"Laziza", ustoz: true };
 student.sinf;
 student.harf;
+student.ism;
 student.ustoz;
+text.innerHTML=student.sinf;
